bc4eaf1fc8d938fa1f95db5c1de2d0bced8ccc69
richardsforu <praveen.somireddy@gmail.com>
2020-11-16 13:23:10 +0530

    ribbon

---
+++ cloud/ribbon-loadbalncing-ws/2-3-FlightSearch-Service/src/main/java/com/cts/brownfield/pss/controller/SearchRestController.java
@@ -0,0 +1,58 @@
+package com.cts.brownfield.pss.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cts.brownfield.pss.entity.Flight;
+import com.cts.brownfield.pss.service.SearchQuery;
+import com.cts.brownfield.pss.service.SearchService;
+
+@RestController
+@RequestMapping("/api/pss")
+@CrossOrigin
+@RefreshScope
+public class SearchRestController {
+
+	@Value("${originairports.shutdown}")
+	private String originAirportShutdownList;
+
+	@Autowired
+	private SearchService searchService;
+
+	@PostMapping("/findFlights")
+	public List<Flight> searchFlights(@RequestBody SearchQuery searchQuery) {
+		System.out.println(">>>>>> Search Service Three <<<<<<");
+
+		System.out.println(searchQuery);
+
+		// ....
+
+		if (Arrays.asList(originAirportShutdownList.split(",")).contains(searchQuery.getOrigin())) {
+			System.out.println(">>>>>>>> The origin airport is in shutdown state <<<<<<<<<<<<");
+			return new ArrayList<Flight>();
+		}
+
+		return searchService.search(searchQuery);
+	}
+
+	@GetMapping("/findFlight/{id}")
+	public Flight findFlight(@PathVariable("id") long id) {
+		System.out.println(">>>>>> Search Service Three <<<<<<");
+
+		return searchService.findByFlightId(id);
+		
+	}
+
+}
---
+++ cloud/ribbon-loadbalncing-ws/Config-Server/src/main/java/com/cts/ConfigServerApplication.java
@@ -0,0 +1,21 @@
+package com.cts;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+	
+	
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(ConfigServerApplication.class, args);
+		
+
+	}
+
+}
